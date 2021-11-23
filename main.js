5d7d7115baf2a29800d83fa63430c1a8ec829af8
Violeta Aguirre <71351678+heyvilo@users.noreply.github.com>
2021-11-22 23:49:38 -0300

    DOM (todavía con fallas)

--- main.js
+++ main.js
@@ -65,3 +65,19 @@ const evento = (cantidadHoras) =>{
     ]
 
 console.log(ciclosNocturna);
+
+
+//Almacenar datos de contacto - DOM
+
+function contactar(){
+    let nombre = document.getElementById ("nombre");
+    let email = document.getElementById ("email");
+    let tel = document.getElementById ("tel");
+    let mensaje =document.getElementsByClassName ("inputMensaje");
+
+    console.log(nombre.value)
+    console.log(email.value)
+    console.log(tel.value)
+    console.log(mensaje.value)
+
+}
--- secciones/main.js
+++
@@ -1,6 +0,0 @@
-let entradaGeneral = 200
-let entradaEstudiantes = 100
-let entradasJubilados = 100
-let entradasDiscapacitados = 0
-let entradasAmigos = 100
-
