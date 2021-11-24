8835aef79a63192d22e60c943bb2293d9d25b1dd
Violeta Aguirre <71351678+heyvilo@users.noreply.github.com>
2021-11-24 15:11:24 -0300

    dom

--- contacto.js
+++ contacto.js
@@ -1,16 +1,19 @@
 //Almacenar datos de contacto - DOM
 
 function contactar(){
-    let nombre = document.getElementById ("nombre");
-    let email = document.getElementById ("email");
-    let tel = document.getElementById ("tel");
+    let nombre = document.getElementsByClassName("inputForm");
+    let email = document.getElementsByClassName ("inputForm");
+    let tel = document.getElementsByClassName ("inputForm");
     let mensaje =document.getElementsByClassName ("inputMensaje");
+
+    alert("Mensaje enviado")
+
+
 
     console.log(nombre.value)
     console.log(email.value)
     console.log(tel.value)
     console.log(mensaje.value)
 
-    alert("Mensaje enviado")
 
 }
