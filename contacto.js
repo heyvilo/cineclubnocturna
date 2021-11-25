f117118e442a3bc21fe9f907db6df09d32611fc4
Violeta Aguirre <71351678+heyvilo@users.noreply.github.com>
2021-11-24 22:14:31 -0300

    POR FIN

--- contacto.js
+++ contacto.js
@@ -1,19 +1,36 @@
 //Almacenar datos de contacto - DOM
 
-function contactar(){
-    let nombre = document.getElementsByClassName("inputForm");
-    let email = document.getElementsByClassName ("inputForm");
-    let tel = document.getElementsByClassName ("inputForm");
-    let mensaje =document.getElementsByClassName ("inputMensaje");
+//Tuve que agregar un preventDefault porque el botón de enviar me redireccionaba solo y no me imprimía por consola
 
+document.getElementById("botoncito").addEventListener("reset", function(event){
+    event.preventDefault()
+  });
+
+
+
+/* Al no tener login, voy a aplicar los conceptos de DOM a la página de Contacto, capturando
+los datos y el mensaje que deja el seguidor, para de esa manera contactarlo en el futuro. 
+
+Al hacer click ENVIAR, en ese evento almaceno el value de todos los datos que me proporcionó, envío 
+un alert de que se envío la consulta correctamente y "guardo" sus datos en un array. */
+
+function contactar(){
+
+    let nombre = document.getElementById("name");
+    let email = document.getElementById ("email");
+    let tel = document.getElementById ("tel");
+    let mensaje =document.getElementById ("mensaje")
+    
     alert("Mensaje enviado")
 
+    let contactoGuardado = []
 
+    contactoGuardado.push(nombre.value)
+    contactoGuardado.push(email.value)
+    contactoGuardado.push(tel.value)
+    contactoGuardado.push(mensaje.value)
 
-    console.log(nombre.value)
-    console.log(email.value)
-    console.log(tel.value)
-    console.log(mensaje.value)
-
-
+    console.log(contactoGuardado)
+    
 }
+
